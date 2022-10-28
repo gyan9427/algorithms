208484f0c0cbf4f62fd6c0c73dd2177722209e05
gyan <gyan9427@gmail.com>
2022-10-28 21:56:43 +0530

    :

--- tree/index.js
+++ tree/index.js
@@ -1,11 +1,14 @@
 var tree;
+let treeArr = [3,8,6,1,9,14]
 function setup() {
     noCanvas();
     tree = new Tree();
-    var n = new Node(5);
-    tree.addNode(n);
+    // tree.addValue(5);
+    treeArr.forEach(val=>{
+        tree.addValue(val);
+    })
     console.log(tree);
-    console.log("hello there")
+    // console.log("hello there")
 }
 
 function draw() {
@@ -18,15 +21,31 @@ class Node{
         this.left = null;
         this.right = null;
     }
+    addNode(n){
+        if(n.value < this.value){
+            if(this.left == null){
+                this.left = n;
+            }
+            this.left.addNode(n);
+        }else if(n.value > this.value){
+            if(this.right == null){
+                this.right = n;
+            }
+            this.right.addNode(n);
+        }
+    }
 }
 
 class Tree{
     constructor(){
         this.root = null;
     }
-    addNode(n){
+    addValue(val){
+        let n = new Node(val)
         if(!this.root){
             this.root = n; 
+        }else{
+            this.root.addNode(n)
         }
     }
 }
