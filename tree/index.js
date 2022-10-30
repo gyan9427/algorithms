08e3a1d06858368de4939a44833f23cd3c5b8452
gyan <gyan9427@gmail.com>
2022-10-30 20:33:17 +0530

    add left right positions

--- tree/index.js
+++ tree/index.js
@@ -1,15 +1,27 @@
 var tree;
+let mid = 0;
+let y = 0;
+let yr = 0;
+let yl = 0;
 let treeArr = [3,8,6,1,9,14]
 function setup() {
-    noCanvas();
+    // noCanvas();
+    createCanvas(600,400);
+    background(51);
+    mid = width/2; 
     tree = new Tree();
     // tree.addValue(5);
     treeArr.forEach(val=>{
-        tree.addValue(val);
+        tree.addValue(Math.floor(Math.random()*100));
     })
     tree.traverse()
     // console.log(tree);
-    tree.search(8)
+    if(tree.search(8)){
+        console.log("found");
+    }
+    else{
+        console.log("not found");
+    }
     // console.log("hello there")
 }
 
@@ -18,22 +30,32 @@ function draw() {
 }
 
 class Node{
-    constructor(val){
+    constructor(val,x,y){
         this.value = val;
         this.left = null;
         this.right = null;
+        this.x = x;
+        this.y = y;
     }
-    addNode(n){
+    addNode(n,midPrt){
         if(n.value < this.value){
+            mid = (midPrt - 0)/2;
             if(this.left == null){
                 this.left = n;
+                this.left.x = mid;
+                yl+=16;
+                this.left.y = yl;
             }
-            this.left.addNode(n);
+            this.left.addNode(n,mid);
         }else if(n.value > this.value){
+            mid =(width/2)+ (width - (width/2))/2;
             if(this.right == null){
                 this.right = n;
+                this.right.x = mid;
+                yr+=16;
+                this.right.y = yr;
             }
-            this.right.addNode(n);
+            this.right.addNode(n,mid);
         }
     }
     visit(){
@@ -41,13 +63,16 @@ class Node{
             this.left.visit();
         }
         console.log(this.value);
+        fill(255);
+        noStroke();
+        text(this.value,this.x,this.y);
         if(this.right !== null){
             this.right.visit();
         }
     }
     search(val){
         if(this.value === val){
-            return val;
+            return this;
         }else if(val < this.value && this.left != null){
             return this.left.search(val);
         }else if(val > this.value && this.right != null){
@@ -65,9 +90,14 @@ class Tree{
     addValue(val){
         let n = new Node(val)
         if(!this.root){
-            this.root = n; 
+            this.root = n;
+            this.root.x = mid;
+            y+=16
+            yr+=16;
+            yl+=16;
+            this.root.y = y;
         }else{
-            this.root.addNode(n)
+            this.root.addNode(n,mid)
         }
     }
     traverse(){
@@ -75,6 +105,6 @@ class Tree{
     }
     search(val){
         let found = this.root.search(val)
-        console.log("FOUND...",found);
+        return found;
     }
 }
