a2a51b79748f298d07f34a813b1019e62618aa49
gyan <gyan9427@gmail.com>
2022-11-06 23:11:53 +0530

    tree visualizer complete

--- tree/index.js
+++ tree/index.js
@@ -3,7 +3,10 @@ let mid = 0;
 let y = 0;
 let yr = 0;
 let yl = 0;
-let treeArr = [3,8,6,1,9,14]
+let treeLength = Math.floor(Math.random()*20)
+let i = 0;
+let lstep = 100;
+let rstep = 100;
 function setup() {
     // noCanvas();
     createCanvas(600,400);
@@ -11,18 +14,22 @@ function setup() {
     mid = width/2; 
     tree = new Tree();
     // tree.addValue(5);
-    treeArr.forEach(val=>{
+    // treeArr.forEach(val=>{
+    //     tree.addValue(Math.floor(Math.random()*100));
+    // })
+    // for(let i=0;i<treeLength;i++){
+    //     tree.addValue(Math.floor(Math.random()*100));
+    // }
+    let startGraphing = setInterval(()=>{
         tree.addValue(Math.floor(Math.random()*100));
-    })
-    tree.traverse()
-    // console.log(tree);
-    if(tree.search(8)){
-        console.log("found");
-    }
-    else{
-        console.log("not found");
-    }
-    // console.log("hello there")
+        tree.traverse();
+        i++;
+        // console.log(new p5.Delay())
+        if(i>treeLength){
+            clearInterval(startGraphing);
+        }
+    },50)
+    
 }
 
 function draw() {
@@ -36,38 +43,51 @@ class Node{
         this.right = null;
         this.x = x;
         this.y = y;
+        this.traversed = false;
     }
-    addNode(n,midPrt){
+    addNode(n){
         if(n.value < this.value){
-            mid = (midPrt - 0)/2;
+            // mid = (midPrt - 0)/2;
             if(this.left == null){
                 this.left = n;
-                this.left.x = mid;
-                yl+=16;
-                this.left.y = yl;
+                this.left.x = this.x-(lstep);
+                lstep-=16;
+                this.left.y = this.y+20;
             }
-            this.left.addNode(n,mid);
+            this.left.addNode(n);
         }else if(n.value > this.value){
-            mid =(width/2)+ (width - (width/2))/2;
+            // mid =(width/2)+ (width - (width/2))/2;
             if(this.right == null){
                 this.right = n;
-                this.right.x = mid;
-                yr+=16;
-                this.right.y = yr;
+                this.right.x = this.x+(rstep--);
+                rstep-=16;
+                this.right.y = this.y+20;
             }
-            this.right.addNode(n,mid);
+            this.right.addNode(n);
         }
     }
-    visit(){
+    visit(parent){
+        
         if(this.left !== null){
-            this.left.visit();
+            this.left.visit(this);
+            console.log("parent",parent.x)
         }
         console.log(this.value);
-        fill(255);
-        noStroke();
-        text(this.value,this.x,this.y);
+        if(!this.traversed){
+            fill(255);
+            noStroke();
+            textAlign(CENTER)
+            text(this.value,this.x,this.y);
+            stroke(255);
+            line(parent.x,parent.y,this.x,this.y);
+            noFill();
+            ellipse(this.x,this.y,30,30);
+            
+        }
+        this.traversed = true;
+        
         if(this.right !== null){
-            this.right.visit();
+            this.right.visit(this);
         }
     }
     search(val){
@@ -91,17 +111,17 @@ class Tree{
         let n = new Node(val)
         if(!this.root){
             this.root = n;
-            this.root.x = mid;
+            this.root.x = width/2;
             y+=16
             yr+=16;
             yl+=16;
-            this.root.y = y;
+            this.root.y = 16;
         }else{
-            this.root.addNode(n,mid)
+            this.root.addNode(n)
         }
     }
     traverse(){
-        this.root.visit();
+        this.root.visit(this.root);
     }
     search(val){
         let found = this.root.search(val)
